82c70f5a077bdc85b44e8d42a7628c1c16d8dbcd
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-10-08 22:19:08 +0300

    Create program

---
+++ PathOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathSelect
+{
+    class PathOperation
+    {
+        public static string FileNameWithoutExtencion(string path)
+        {
+            if (path.IndexOf('\\') == -1 || path.IndexOf('.') == -1)
+                throw new ArgumentException("Path is incorrect");
+
+            int position = path.LastIndexOf('\\') + 1;
+            return path.Substring(position, path.LastIndexOf('.') - position);
+        }
+
+        public static string RootFoulderName(string path)
+        {
+            if (path.IndexOf(":\\") == -1 || path.IndexOf('\\') == -1)
+                throw new ArgumentException("Path is incorrect");
+
+            int position = path.IndexOf(":\\") + 2;
+            return path.Substring(position, path.IndexOf("\\", position) - position);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PathSelect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine($"Name file without extension: {PathOperation.FileNameWithoutExtencion(path)}");
+                Console.WriteLine($"Root foulder name: {PathOperation.RootFoulderName(path)}");
+            }
+            catch(ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
